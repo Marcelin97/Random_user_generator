dbb4a69953f46f9c0ee2345e2fa12efcae5d4892
Loïs <lois_m@outlook.com>
2022-01-28 12:08:43 +0100

    End of get random user with vue.js

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World</h1>',
+    data() {
+        return {
+          firstName: "Loïs",
+          lastName: " Marcelin",
+          email: "lois_m@outlook.com",
+          gender: "female",
+          picture:
+            "https://randomuser.me/api/portraits/women/10.jpg",
+        };
+    },
+    methods: {
+       async getUser() {
+            const res = await fetch("https://randomuser.me/api")
+            const { results } = await res.json()
+
+            this.firstName =  results[0].name.first
+          this.lastName = results[0].name.last;
+          this.email = results[0].email;
+          this.gender = results[0].gender;
+          this.picture = results[0].picture.large;
+        },
+    }
 })
 
 app.mount('#app')
